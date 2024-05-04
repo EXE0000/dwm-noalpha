8e2a522f8b9fd2a76a05abe588f6a64b576e915e
jh <hjh20080819@outlook.com>
2024-05-04 09:59:47 +0800

    dwm-fancybar-6.2.diff

--- dwm.c
+++ dwm.c
@@ -710,7 +710,7 @@ dirtomon(int dir)
 void
 drawbar(Monitor *m)
 {
-	int x, w, tw = 0, tw, mw, ew = 0;
+	int x, w, tw = 0, mw, ew = 0;
 	int boxs = drw->fonts->h / 9;
 	int boxw = drw->fonts->h / 6 + 2;
 	unsigned int i, occ = 0, urg = 0, n = 0;
