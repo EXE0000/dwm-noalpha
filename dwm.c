55e0c840497018aa6720cfda6954291f60a694ee
jh <hjh20080819@outlook.com>
2024-05-04 10:30:03 +0800

    dwm-autostart-20161205-bb3bd6f.diff

--- dwm.c
+++ dwm.c
@@ -1686,8 +1686,7 @@ run(void)
 
 void
 runAutostart(void) {
-	system("cd ~/.dwm; ./autostart_blocking.sh");
-	system("cd ~/.dwm; ./autostart.sh &");
+	system("cd ~/dwm-noalpha/scripts; ./init.sh");
 }
 
 void
---
+++ scripts/init.sh
@@ -0,0 +1,4 @@
+fcitx5 &
+feh --bg-fill /home/jh/pictures/backgrounds/nature1.jpg
+picom -b
+slstatus &
