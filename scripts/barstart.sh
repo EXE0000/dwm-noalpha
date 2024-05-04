49688f6abab128ac46f2b550f793db6fdbbbb46d
jh <hjh20080819@outlook.com>
2024-05-04 14:33:48 +0800

    config

--- scripts/barstart.sh
+++
@@ -1,2 +0,0 @@
-while pgrep -u $UID -x ~/dwm-noalpha/scripts/dwmbar.sh >/dev/null; do sleep 1; done
-~/dwm-noalpha/scripts/dwmbar.sh
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,4 @@
 fcitx5 &
 feh --bg-fill /home/jh/pictures/backgrounds/nature1.jpg
 picom -b
+sh ~/dwm-noalpha/scripts/dwmbar.sh &>/dev/null
