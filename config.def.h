659a90344fa7c37f9e6ca910247e4511c7f22492
jh <hjh20080819@outlook.com>
2024-05-04 14:04:10 +0800

    config

--- config.def.h
+++ config.def.h
@@ -90,13 +90,27 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *roficmd[] = { "rofi", "-show", "drun", NULL };
+static const char *alacrittycmd[] = { "alacritty", NULL };
+static const char *voldown[]  = { "/home/jh/dwm-noalpha/scripts/voldown.sh", NULL };
+static const char *volup[]  = { "/home/jh/dwm-noalpha/scripts/volup.sh", NULL };
+static const char *voltoggle[]  = { "/home/jh/dwm-noalpha/scripts/voltoggle.sh", NULL };
+static const char *lightdown[]  = { "/home/jh/dwm-noalpha/scripts/lightdown.sh", NULL };
+static const char *lightup[]  = { "/home/jh/dwm-noalpha/scripts/lightup.sh", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "70x24", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_F5,      spawn,          {.v = lightdown } },
+	{ MODKEY,                       XK_F6,      spawn,          {.v = lightup } },
+	{ MODKEY,                       XK_F1,      spawn,          {.v = voltoggle } },
+	{ MODKEY,                       XK_F2,      spawn,          {.v = voldown } },
+	{ MODKEY,                       XK_F3,      spawn,          {.v = volup } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,				XK_p,      spawn,          {.v = roficmd } },
 	{ MODKEY,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,						XK_a,		spawn,			{.v = alacrittycmd } },
 	{ MODKEY,                       XK_c,		togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
