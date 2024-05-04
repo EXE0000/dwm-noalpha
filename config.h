9cd322b350a3b89d3935c6b015e5c1a26b9a08f9
jh <hjh20080819@outlook.com>
2024-05-04 10:37:59 +0800

    font and theme

--- config.h
+++ config.h
@@ -18,7 +18,6 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#37374f";
 #include "themes/nord.h"
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
