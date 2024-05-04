163d622accf0db20059fb6860f2766e628c714d9
jh <hjh20080819@outlook.com>
2024-05-04 18:42:34 +0800

    config

--- config.def.h
+++ config.def.h
@@ -100,6 +100,7 @@ static const char *lightup[]  = { "/home/jh/dwm-noalpha/scripts/lightup.sh", NUL
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "70x24", NULL };
 static const char *trayer[] = { "/home/jh/dwm-noalpha/scripts/trayer.sh", NULL };
+static const char *close_trayer[] = { "/home/jh/dwm-noalpha/scripts/close_trayer.sh", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -109,6 +110,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_F2,      spawn,          {.v = voldown } },
 	{ MODKEY,                       XK_F3,      spawn,          {.v = volup } },
 	{ MODKEY,                       XK_z,      spawn,          {.v = trayer } },
+	{ MODKEY|ShiftMask,             XK_z,      spawn,          {.v = close_trayer } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,				XK_p,      spawn,          {.v = roficmd } },
 	{ MODKEY,             XK_Return, spawn,          {.v = termcmd } },
