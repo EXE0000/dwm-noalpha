1605f6dee7fefd6358af631f02c71f3d6665163a
jh <hjh20080819@outlook.com>
2024-05-04 09:47:14 +0800

    dwm-dwm-gaplessgrid-20160731-56a31dc.diff

--- config.def.h
+++ config.def.h
@@ -38,11 +38,13 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+#include "gaplessgrid.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "[G]",      gaplessgrid },
 };
 
 /* key definitions */
