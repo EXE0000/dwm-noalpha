7df0ac6de2d09d0b5b086441636e166012c11781
jh <hjh20080819@outlook.com>
2024-05-05 11:44:25 +0800

    config

--- scripts/dwmbar.sh
+++ scripts/dwmbar.sh
@@ -50,7 +50,7 @@ dwm_cpu() {
 	read cpu a b c idle rest </proc/stat
 	total=$((a + b + c + idle))
 	cpu=$((100 * ((total - prevtotal) - (idle - previdle)) / (total - prevtotal)))
-	echo -e " $cpu% cpu"
+	echo -e " $cpu%"
 }
 
 print_mem() {
