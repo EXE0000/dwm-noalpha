c6cb119f02ceb189e43b7bc0459e798705e5014d
jh <hjh20080819@outlook.com>
2024-05-19 09:54:52 +0800

    fix

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 fcitx5 &
-feh --bg-fill /home/jh/pictures/backgrounds/nature1.jpg
+feh --bg-fill /home/jh/pictures/backgrounds/china1.jpg
 picom -b
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 ~/dwm-noalpha/scripts/dwmbar.sh &
